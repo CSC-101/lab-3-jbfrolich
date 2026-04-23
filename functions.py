e4dcce01c8758e2a066ee9c8d76308a8bf928ab1
jbfrolich <jfrolich@calpoly.edu>
2026-04-23 14:25:53 -0700

    Changed n * n to n * 2 to make n be doubled instead of squared

--- functions.py
+++ functions.py
@@ -2,5 +2,5 @@
 # Input: a number to be doubled
 # Result: a number
 def double(n:int) -> int:
-    result = n * n
+    result = n * 2          #result should be n doubled not squared
     return result
